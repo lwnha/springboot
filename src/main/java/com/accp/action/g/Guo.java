8144766aed97521157b4489780053d9fb70b885c
liuwen <wen@liuwen.com>
2019-01-04 10:56:21 +0800

    第一次提交boot项目

---
+++ src/main/java/com/accp/action/g/Guo.java
@@ -0,0 +1,11 @@
+package com.accp.action.g;
+
+public class Guo {
+	
+	public static void main(String[] args) {
+		System.out.println("123");
+		System.out.println("123");
+		System.out.println("123");
+		System.out.println("123");
+	}
+}
